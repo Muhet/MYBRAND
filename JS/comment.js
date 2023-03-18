24596a8510f28cca2a4a7f7559263da768171202
Muhet <muhedarius96@gmail.com>
2023-03-19 01:28:30 +0200

    fixing comments retrieve on admin side

--- JS/comment.js
+++ JS/comment.js
@@ -26,7 +26,7 @@ const fetch_comment = async () => {
     fetch(`https://excited-visor-hen.cyclic.app/api/blogs`)
     .then((res) => res.json())
     .then((Blog) =>{
-   console.log(Blog.data[0])
+   console.log(Blog.data[{_id}])
    
    fetch(`https://excited-visor-hen.cyclic.app/api/comments`)
         .then((response) => response.json())
