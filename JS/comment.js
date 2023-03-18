0fb0a30b217f64d78846430b23e53bea822c2f86
Muhet <muhedarius96@gmail.com>
2023-03-19 00:19:32 +0200

    fixing comments retrieve on admin side

--- JS/comment.js
+++ JS/comment.js
@@ -70,9 +70,10 @@ const deletecoment = async (article_id) => {
 
 }
 const viewComment = async (id) => {
-    const ViewContent = document.querySelector('#View_comment');
     const response = await fetch(`https://excited-visor-hen.cyclic.app/api/blogs`);
     const res = await fetch(`http://localhost:3000/api/api/comments/?id=${id}`);
+    const ViewContent = document.querySelector('#View_comment');
+    
     const Blog = await res.json();
     console.log(Blog)
     let temp = "";
