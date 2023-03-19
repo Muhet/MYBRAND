d6177d066a1dfabc4c1a19ce30d8e6ad8778d870
Muhet <muhedarius96@gmail.com>
2023-03-19 02:16:47 +0200

    fixing comments retrieve on admin side

--- JS/comment.js
+++ JS/comment.js
@@ -49,7 +49,7 @@ const fetch_comment = async () => {
 </div>
       <div class="table_cell last_cell">
           <div class="actionIcons">
-              <a href="viewComent.html/id=${id}">
+              <a href="viewComent.html/id=${comment._id}">
               <img src="../images/ViewsIcon.png" alt="" id="editIcon""/></a>
               <img src="../images/Delete.png" alt="" id="deleteIcon" onclick="deletecoment('${comment._id}');"/>
           </div>
