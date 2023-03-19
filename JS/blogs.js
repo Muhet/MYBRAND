2933673a0510c814a3f32d0c1b0d9064222f00fa
Muhet <muhedarius96@gmail.com>
2023-03-19 13:25:35 +0200

    fixing message from client

--- JS/blogs.js
+++ JS/blogs.js
@@ -94,7 +94,7 @@ const fetch_clientBlog = async () => {
                              <span id="blogparagraph">${blog.description.slice(0, 50)}
                              </span>
                              <div class="ReadMore" >
-                                 <a href="ReadmoreBlog.html?id=${blog._id}" onClick='renderBlog()' id="blogReadMore">Read More</a>
+                                 <a onclick="fetch_singleBlog('${blog._id}')" onClick='renderBlog()' id="blogReadMore">Read More</a>
                              </div>
                          </div>
                          </div>
